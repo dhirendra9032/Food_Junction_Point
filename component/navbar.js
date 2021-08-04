089676547d19fe888873589529cd032bf5f570a2
Dhirendra Kumar <dhirendra9032@gmail.com>
2021-08-05 02:22:29 +0530

    Food junction website

---
+++ component/navbar.js
@@ -0,0 +1,18 @@
+function navbar(){
+    return `<nav class="navbar">
+
+    <img src="https://www.transparentpng.com/thumb/sushi/FjiXtn-sushi-food-picture.png"
+        alt="Sushi Food Picture @transparentpng.com" width="150px" height="100px">
+   
+    <input type="text" placeholder="Search for Receipi" id="s_data">
+    <button id="btn">Search</button>
+    <h3 id="Day">Receipi of the Day</h3>
+    <h3 id="latest">Latest Receipi</h3>
+
+   </nav>
+   <div id="data_div">
+   
+   </div>`;
+}
+
+export default navbar();
