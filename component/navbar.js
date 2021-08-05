c12c75775298073c645dbfa4c79622133192bb63
Dhirendra Kumar <dhirendra9032@gmail.com>
2021-08-06 00:23:17 +0530

    Recipi Detail added

--- component/navbar.js
+++ component/navbar.js
@@ -4,10 +4,10 @@ function navbar(){
     <img src="https://www.transparentpng.com/thumb/sushi/FjiXtn-sushi-food-picture.png"
         alt="Sushi Food Picture @transparentpng.com" width="150px" height="100px">
    
-    <input type="text" placeholder="Search for Receipi" id="s_data">
+    <input type="text" placeholder="Search for Recipi" id="s_data">
     <button id="btn">Search</button>
-    <h3 id="Day">Receipi of the Day</h3>
-    <h3 id="latest">Latest Receipi</h3>
+    <h3 id="Day">Recipis of the Day</h3>
+    <h3 id="latest">Latest Recipi</h3>
 
    </nav>
    <div id="data_div">
